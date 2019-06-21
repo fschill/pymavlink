54f0c484eef35e6add03ed5291c68080d8270aa4
Felix Schill <felix.schill@hydromea.com>
2019-06-21 13:39:36 +0200

    changed default dialect to auv to enable windows build

--- mavutil.py
+++ mavutil.py
@@ -40,7 +40,7 @@ global_link_id = 0
 
 # Use a globally-set MAVLink dialect if one has been specified as an environment variable.
 if not 'MAVLINK_DIALECT' in os.environ:
-    os.environ['MAVLINK_DIALECT'] = 'common'
+    os.environ['MAVLINK_DIALECT'] = 'auv'
 
 def mavlink10():
     '''return True if using MAVLink 1.0 or later'''
